d6e7659b8393675d948e6009b95244c662356f74
AlexxalexS <37020080+AlexxalexS@users.noreply.github.com>
2021-09-30 16:08:55 +0300

    Initial Commit

---
+++ SaveYourData/SaveYourDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveYourDataApp.swift
+//  SaveYourData
+//
+//  Created by Alexey on 30.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SaveYourDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
