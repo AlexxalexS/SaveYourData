0653ea5fc0f8c6a2e298be84c8c3c9cd2ef77b65
a.sarantsev <37020080+AlexxalexS@users.noreply.github.com>
2022-04-24 18:41:05 +0300

    feat: new api value from personal VDS

--- SaveYourData/Service/API.swift
+++ SaveYourData/Service/API.swift
@@ -3,6 +3,7 @@ let APIServiceEnvironment: APIEnvironment = .server
 enum APIEnvironment: String {
 
     case local = "http://localhost:8080"
-    case server = "https://new-server-totp.herokuapp.com"
+    case server = "http://31.184.253.231"
+    case heroku = "https://new-server-totp.herokuapp.com"
 
 }
